ae0065cd62c1008582102fa15d2e1bdfd3c1d852
pRostok <rostmart29@gmail.com>
2022-08-18 00:19:38 -0700

    apply styling, add duplicateDivs function

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+
+const total = document.getElementById('total')
+ 
+const duplicateDivs = (a) => {
+    
+    let DivTag = '';
+   
+    for (let i = 1; i <= a; i++){
+        DivTag += `<div class="cat" id='cat${i}' style="background: url(folder/c${i}.webp) no-repeat center center/cover;"></div>`;
+        
+    }
+    document.getElementById('cats').innerHTML = DivTag;       
+};
+
+duplicateDivs(16)
+
 const divs = document.querySelectorAll('div')
 
 divs.forEach(div => {
+    
+    const indClick = document.createElement('span')   
 
-    const clock = document.createElement('span')
     let count = 0
+    div.appendChild(indClick)
+    indClick.classList.add('times')
+    indClick.innerHTML = count   
+    
+    const icon = document.createElement('i')
+    icon.classList.add('fa-brands')
+    icon.classList.add('fa-gratipay')
+    div.appendChild(icon)
+
 
     div.addEventListener('dblclick', function createHeart(e) {
         
@@ -13,8 +39,9 @@ divs.forEach(div => {
 
         const x = e.clientX
         const y = e.clientY
+
         const leftOffset = e.target.offsetLeft
-        const topOffset = e.target.offsetTop
+        const topOffset = e.offsetTop
 
         const xInside = x - leftOffset
         const yInside = y - topOffset
@@ -23,82 +50,19 @@ divs.forEach(div => {
         heart.style.left = `${xInside}px`
 
         div.appendChild(heart)
-        setTimeout(() => heart.remove(), 500)   
-
+        setTimeout(() => heart.remove(), 500)  
 
         count++
-
-        div.appendChild(clock)     
-        clock.classList.add('times')
-        clock.innerHTML = count
-                                  
-    }) 
-       
+        indClick.innerHTML = count      
+        
+        total.innerHTML = ++total.innerHTML
+           
+    })     
 })
 
 
 
 
-// const divs = document.querySelectorAll('div')
 
-// const arr = [divs]
-// console.log('zero log', arr)              /* Zero log */
 
-// for (let i = 0; i < divs.length; i++) {
 
-//     console.log('first log', i)                         /* First log */
-//     const clock = document.createElement('span')
-
-//     let count = 0
-
-//     divs[i].addEventListener('dblclick', (e) => {
-//         ////////////////////////////////////////////////////////////////////////////////////////
-//         const heart = document.createElement('i')
-//         heart.classList.add('fas')
-//         heart.classList.add('fa-heart')
-
-//         const x = e.clientX
-//         const y = e.clientY
-//         const leftOffset = e.target.offsetLeft
-//         const topOffset = e.target.offsetTop
-//         /*function createHeart*/
-//         const xInside = x - leftOffset
-//         const yInside = y - topOffset
-
-//         heart.style.top = `${yInside}px`
-//         heart.style.left = `${xInside}px`
-
-//         divs[i].appendChild(heart)
-//         setTimeout(() => heart.remove(), 500)
-//         ////////////////////////////////////////////////////////////////////////////////////////
-
-//         count++;
-
-//         divs[i].appendChild(clock)
-//         clock.classList.add('times')
-//         clock.innerHTML = count
-
-//         console.log('second log', i)                     /* Second log */
-//     })
-
-// }
-
-// function createHeart(e) {
-//     const heart = document.createElement('i')
-//     heart.classList.add('fas')
-//     heart.classList.add('fa-heart')
-
-//     const x = e.clientX
-//     const y = e.clientY
-//     const leftOffset = e.target.offsetLeft
-//     const topOffset = e.target.offsetTop
-
-//     const xInside = x - leftOffset
-//     const yInside = y - topOffset
-
-//     heart.style.top = `${yInside}px`
-//     heart.style.left = `${xInside}px`
-
-//     divs[i].appendChild(heart)
-//     setTimeout(() => heart.remove(), 500)
-// }
